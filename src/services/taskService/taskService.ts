39bdc58d7c898c68b103993871adebdc6bffef9b
Umamaheswari-gude <umamaheswari.g@everest.engineering>
2025-11-19 00:11:07 +0530

    Add service layer for fetch tasks

--- src/services/taskService/taskService.ts
+++ src/services/taskService/taskService.ts
@@ -8,3 +8,11 @@ export const addTasks = async (task: Task): Promise<Task> => {
   await addTask.set({ ...task, id: addTask.id });
   return { ...task ,id: addTask.id }
 };
+
+export const getTasks = async (): Promise<Task[]> => {
+  const taskData = await taskCollection.get();
+  return taskData.docs.map((doc) => ({
+    ...doc.data(),
+    id: doc.id,
+  } as Task));
+};

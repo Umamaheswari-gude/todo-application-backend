ff25091d6e0939aeb8c869a7b68b37382238d40c
Umamaheswari-gude <umamaheswari.g@everest.engineering>
2025-11-18 16:59:33 +0530

    Add validation that are fields are required to addTask

--- src/controllers/addTask/addTask.ts
+++ src/controllers/addTask/addTask.ts
@@ -2,6 +2,12 @@ import { addTasks } from "../../services/addTask/addTask"
 import { Request, Response } from "express";
 
 export const addTask = async (req: Request, res: Response ) => {
+    const { name, description, status, priority, deadline } = req.body;
+     if (!name || !description || !status || !priority || !deadline ) {
+    res.status(400).json({
+      message: "All fields are required",
+    });
+}
     try {
         const task = await addTasks(req.body);
         res.status(201).json(task);

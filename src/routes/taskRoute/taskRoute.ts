02f5911dbb7b73be28196159067cca33e7819eaa
Umamaheswari-gude <umamaheswari.g@everest.engineering>
2025-11-19 12:14:35 +0530

    Add delete route

--- src/routes/taskRoute/taskRoute.ts
+++ src/routes/taskRoute/taskRoute.ts
@@ -1,10 +1,11 @@
 import express from "express";
-import { addTask, getTask, updateTask } from "../../controllers/taskController/taskController";
+import { addTask, deleteTask, getTask, updateTask } from "../../controllers/taskController/taskController";
 
 const router = express.Router();
 
 router.post("/", addTask);
 router.get("/", getTask)
 router.put("/:id", updateTask);
+router.delete("/:id", deleteTask)
 
 export default router;
